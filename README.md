0b3ff825be7525c547351310f0304083a38a7da9
liuweia <liuweia@mail.open.com.cn>
2019-03-05 10:35:22 +0800

    添加leetcode算法刷题
    添加leetcode算法刷题

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # leetcode
 leetcode样例
+
+leetcode 刷题
+
+Leetcode算法题
+双指针：https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+排序：
+快速选择、堆排序：https://leetcode.com/problems/kth-largest-element-in-an-array/description/
+桶排序：https://leetcode.com/problems/top-k-frequent-elements/description/
+荷兰国旗问题：https://leetcode.com/problems/sort-colors/description/
+贪心：https://leetcode.com/problems/assign-cookies/description/
+
---
+++ Two_Sum.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+
+# Your returned answers (both index1 and index2) are not zero-based.
+# You may assume that each input would have exactly one solution
+# and you may not use the same element twice.
+
+
+class Solution:
+    """
+    numbers: inputs sorted list
+    target: int
+
+    Example:
+    Input: numbers = [2, 7, 11, 15], target = 9
+    Output: [1,2]
+    Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+    """
+    def twoSum(self, numbers, target):
+        
+        return L
