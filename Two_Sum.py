e55630df08fcd7ab6b0be2591c501d5a37e9eb82
liuweia <liuweia@mail.open.com.cn>
2019-03-05 13:38:33 +0800

    双指针
    双指针

--- Two_Sum.py
+++ Two_Sum.py
@@ -12,9 +12,24 @@ class Solution:
 
     Example:
     Input: numbers = [2, 7, 11, 15], target = 9
-    Output: [1,2]
+    Output: [1, 2]
     Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
     """
     def twoSum(self, numbers, target):
-        
-        return L
+        map = {}
+        for i in range(len(numbers)):
+            if numbers[i] not in map:
+                map[target - numbers[i]] = i
+            else:
+                return map[numbers[i]]+1, i+1
+
+
+if __name__ == '__main__':
+    # numbers = [2, 7, 11, 15]
+    # target = 9
+    numbers = [2, 3, 4]
+    target = 6
+    # numbers = [5, 25, 75]
+    # target = 100
+    a = Solution()
+    print(a.twoSum(numbers, target))
